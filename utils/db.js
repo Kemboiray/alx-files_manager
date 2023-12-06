ac5342e043b244af8a7c1aae46cbe55ddb3e364f
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-12-07 01:47:31 +0300

    Task 1

---
+++ utils/db.js
@@ -0,0 +1,41 @@
+// File: db.js
+// Description: Database connection and configuration. This is handled through a
+// class `DBClient`.
+
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'file_manager';
+    const url = `mongodb://${host}:${port}/${database}`;
+
+    MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+      if (!err) {
+        this.db = client.db();
+        this.users = this.db.collection('users');
+        this.files = this.db.collection('files');
+      } else {
+        this.db = false;
+      }
+    });
+  }
+
+  isAlive() {
+    return this.db !== false;
+  }
+
+  async nbUsers() {
+    const nbUsers = await this.users.countDocuments();
+    return nbUsers;
+  }
+
+  async nbFiles() {
+    const nbFiles = await this.files.countDocuments();
+    return nbFiles;
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
